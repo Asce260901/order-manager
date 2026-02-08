8993952650779cbc39e5d15598b3ecaa7f757ae2
Asce260901 <ajm55@usf.edu>
2026-02-07 22:53:05 -0500

    last product removed, price update and stock update added

--- cc_3b.js
+++ cc_3b.js
@@ -24,10 +24,12 @@ let inventory = [
         stock: 469,
     }
 ];
+
 //One line Summary For Initial Inventory
 inventory.forEach((inventory) =>
     console.log(`${inventory.SKU} | ${inventory.productName} | $${inventory.price} | ${inventory.stock}`)
 );
+
 //New product
 inventory.push({
     SKU: "SKU-6001004",
@@ -35,3 +37,19 @@ inventory.push({
     price: 7.51,
     stock: 633,
 });
+
+//Last product removed and log it 
+console.log("Product removed:", inventory.pop());
+
+//Product price update
+let saleProductName = inventory[1].productName;
+let oldPrice = inventory[1].price;
+let salePrice = inventory[1].price *= (1 - 0.35);
+console.log(`!!BIG SALE!! \n${saleProductName} price have been decreased to \n$${salePrice.toFixed(2)} from ${oldPrice} just for today`);
+
+//Product Stock update
+let restockProdName = inventory[2].productName;
+let oldStock = inventory[2].stock;
+let newStock = inventory[2].stock += 400;
+console.log(`!!Restock!! \nthe product ${restockProdName} have been restocked to ${newStock} from ${oldStock}`)
+
