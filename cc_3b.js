8f867c615d1d5534ca165438ad156d3a3e8c412d
Asce260901 <ajm55@usf.edu>
2026-02-08 00:35:45 -0500

    functions and results added

--- cc_3b.js
+++ cc_3b.js
@@ -44,12 +44,81 @@ console.log("Product removed:", inventory.pop());
 //Product price update
 let saleProductName = inventory[1].productName;
 let oldPrice = inventory[1].price;
-let salePrice = inventory[1].price *= (1 - 0.35);
+let salePrice = inventory[1].price *= (1 - 0.45);
 console.log(`!!BIG SALE!! \n${saleProductName} price have been decreased to \n$${salePrice.toFixed(2)} from ${oldPrice} just for today`);
-
+inventory[1].price = salePrice
 //Product Stock update
 let restockProdName = inventory[2].productName;
 let oldStock = inventory[2].stock;
 let newStock = inventory[2].stock += 400;
 console.log(`!!Restock!! \nthe product ${restockProdName} have been restocked to ${newStock} from ${oldStock}`)
+inventory[2].stock = newStock
+
+// Orders
+let orders = [
+    {
+        orderId: "ORD-7001000",
+        items:[
+            {SKU: "SKU-6001000", quantity:70},
+            {SKU: "SKU-6001003", quantity:12}
+        ]
+    },
+    {
+        orderId: "ORD-7001001",
+        items:[
+            {SKU: "SKU-6001002", quantity:300},
+            {SKU: "SKU-6001001", quantity:200}
+        ]
+    },
+    {
+        orderId: "ORD-7001002",
+        items: [
+            {SKU: "SKU-6001004", quantity:52},
+            {SKU: "SKU-6001002", quantity:12}
+        ]
+    }
+];
+
+//Product finder by SKU function 
+function findproductsBySku(SKU){
+    let product = inventory.find((p) => p.SKU === SKU);
+    return product ? product: null;
+}
+
+//Function for order processing
+function processOrder(order){
+    for (let item of order.items){
+        let product = findproductsBySku(item.SKU);
+
+        if (!product){
+            return `Order ${order.orderId} cannot be completed: SKU ${item.SKU} not found in inventory.`;
+        }
+        if (product.stock < item.quantity){
+            let deficit = item.quantity - product.stock
+            return `Order ${order.orderId} cannot be completed: Product ${product.productName} is short by ${deficit} unit(s).`;
+        }
+    }
+        let total = 0;
+        for (let item of order.items){
+            let product = findproductsBySku(item.SKU);
+            
+            product.stock -= item.quantity;
+            total += product.price * item.quantity;
+        }
+        return `Order ${order.orderId} has been completed: Order total = $${total.toFixed(2)}`;
+}
+
+//Orders result
+console.log(`Order Processing`);
+orders.forEach((order) =>{
+    let result = processOrder(order);
+    console.log(result);
+});
+
+//Reamining stock
+console.log(`Remaining stock after orders`);
+inventory.forEach((p) => {
+    console.log(`${p.SKU} | ${p.productName} | $${p.price} | Stock: ${p.stock}`);
+});
+
 
